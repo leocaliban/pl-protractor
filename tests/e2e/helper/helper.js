a39926bacacc5a222a8855a10f73e2e18ae12887
Leocaliban <leocaliban@gmail.com>
2019-03-17 17:35:36 -0300

    Utiliza shortid

--- tests/e2e/helper/helper.js
+++ tests/e2e/helper/helper.js
@@ -1,6 +1,13 @@
+var shortid = require('shortid');
+
 var uuid = require('node-uuid');
+
 var Helper = function () { };
 Helper.prototype.generateRandomString = function () {
     return uuid.v4();
 };
+
+Helper.prototype.generateRandomEmail = function () {
+    return shortid.generate() + '@gmail.com';
+}
 module.exports = Helper;
